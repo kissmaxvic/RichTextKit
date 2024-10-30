43b1855b311f20651ec0987db1399106de6e64e0
Maxim <64606867+kissmaxvic@users.noreply.github.com>
2024-10-30 22:11:03 +0500

    Update RichTextViewRepresentable.swift

--- Sources/RichTextKit/RichTextViewRepresentable.swift
+++ Sources/RichTextKit/RichTextViewRepresentable.swift
@@ -5,29 +5,6 @@
 //  Created by Daniel Saidi on 2024-03-04.
 //  Copyright © 2024 Daniel Saidi. All rights reserved.
 //
-import SwiftUI
-import UIKit
-
-
-struct TextKit1TextView: UIViewRepresentable {
-    @Binding var text: NSAttributedString
-
-    func makeUIView(context: Context) -> UITextView {
-        // Создаем UITextView с использованием TextKit 1
-        if #available(iOS 16, *) {
-            let textView = UITextView(usingTextLayoutManager: false)
-        }
-        textView.isEditable = true
-        textView.isScrollEnabled = true
-        textView.attributedText = text
-        return textView
-    }
-
-    func updateUIView(_ uiView: UITextView, context: Context) {
-        uiView.attributedText = text
-    }
-}
-
 #if macOS
 import AppKit
 
@@ -39,7 +16,7 @@ public typealias RichTextViewRepresentable = NSTextView
 import UIKit
 
 /// This typealias bridges UIKit & AppKit native text views.
-public typealias RichTextViewRepresentable = TextKit1TextView()
+public typealias RichTextViewRepresentable = UITextView
 #endif
 
 
